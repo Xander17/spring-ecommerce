f01808e681cac1b96ca10ab804342fca52b8604e
Cromag <alseko_2001@mail.ru>
2020-06-24 14:36:47 +0300

    initial commit

---
+++ shop-admin/src/test/java/ru/geekbrains/shopadmin/ShopAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shopadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
