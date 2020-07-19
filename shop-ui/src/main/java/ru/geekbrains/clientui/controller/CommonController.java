c82e4a88a05e764eb2435399dde89cb28beac897
Cromag <alseko_2001@mail.ru>
2020-07-19 20:55:55 +0300

    category list in templates from db

--- shop-ui/src/main/java/ru/geekbrains/clientui/controller/CommonController.java
+++ shop-ui/src/main/java/ru/geekbrains/clientui/controller/CommonController.java
@@ -4,15 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import ru.geekbrains.clientui.service.CartService;
+import ru.geekbrains.clientui.service.CategoryService;
 
 @Component
 @RequiredArgsConstructor
 public class CommonController {
 
     private final CartService cartService;
+    private final CategoryService categoryService;
 
     public void addRequiredModelAttributes(Model model) {
         model.addAttribute("cartSize", cartService.getCartSize());
         model.addAttribute("cartSum", cartService.getRecalculatedSum());
+        model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("cat", "11111");
     }
 }
